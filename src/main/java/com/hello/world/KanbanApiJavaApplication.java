0038316c56d97dc1058c6bc188548ccdc583875a
darnos <darnoslima@gmail.com>
2020-01-09 18:47:32 -0300

    first commit

---
+++ src/main/java/com/hello/world/KanbanApiJavaApplication.java
@@ -0,0 +1,13 @@
+package com.hello.world;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanApiJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanbanApiJavaApplication.class, args);
+	}
+
+}
