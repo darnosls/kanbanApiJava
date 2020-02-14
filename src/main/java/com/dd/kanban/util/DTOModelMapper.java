ffcf26e2e43a5607c22e8746ce058008afcca02c
darnos <darnoslima@gmail.com>
2020-02-14 15:01:22 -0300

    dtos and mappers

--- src/main/java/com/dd/kanban/util/DTOModelMapper.java
+++ src/main/java/com/dd/kanban/util/DTOModelMapper.java
@@ -1,7 +1,23 @@
 package com.dd.kanban.util;
 
+import javax.persistence.EntityManager;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.mvc.method.annotation.RequestResponseBodyMethodProcessor;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.Collections;
+
 public class DTOModelMapper extends RequestResponseBodyMethodProcessor {
+	private static final ModelMapper modelMaper = new ModelMapper();
+	
+	private EntityManager entityManager;
+
+	public DTOModelMapper(ObjectMapper objectMapper, EntityManager entityManger) {
+		super(Collections.singletonList(new MappingJackson2HttpMessageConverter(objectMapper)));
+		this.entityManager = entityManager;
+	}
 
 }
